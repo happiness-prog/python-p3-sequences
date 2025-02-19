e8bc98377adf126b4c5cb31ddd0b6953d24a04ff
Happiness Maina <mainahappiness361@gmail.com>
2025-02-19 09:18:22 +0300

    file

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    fibonacci = [0, 1] if length > 1 else [0]
+    
+    for i in range(2, length):
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci)
+    
